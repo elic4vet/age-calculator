3252d8f091c760e7af04d5d23a3fedda52d0bfea
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-08-27 14:42:16 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,55 @@
+let button = document.getElementById("btn");
+let inputDay = document.getElementById("day");
+let inputMonth = document.getElementById("month");
+let inputYear = document.getElementById("year");
+
+button.addEventListener("click", function () {
+  let day = inputDay.value;
+  let month = inputMonth.value;
+  let year = inputYear.value;
+
+  let date = new Date(year, month - 1, day); // month - 1 because month starts from 0
+  let yearOfBirth = date.getFullYear();
+  let monthOfBirth = date.getMonth() + 1; // month + 1 because month starts from 0
+  let dayOfBirth = date.getDate();
+
+  let currentDate = new Date();
+  let currentYear = currentDate.getFullYear();
+  let currentMonth = currentDate.getMonth() + 1;
+  let currentDay = currentDate.getDate();
+
+  let years = currentYear - yearOfBirth;
+  if (currentYear < yearOfBirth) {
+    years--;
+  } else if (currentYear == yearOfBirth) {
+    if (currentYear < yearOfBirth) {
+      years--;
+    }
+  }
+
+  let months = currentMonth - monthOfBirth;
+  if (currentMonth < monthOfBirth) {
+    months += 12;
+  } else if (currentMonth == monthOfBirth) {
+    if (currentMonth < monthOfBirth) {
+      months += 12;
+    }
+  }
+
+  let days = currentDay - dayOfBirth;
+  if (currentDay < dayOfBirth) {
+    days += 30;
+  } else if (currentDay == dayOfBirth) {
+    if (currentDay < dayOfBirth) {
+      days += 30;
+    }
+  }
+
+  let result_years = document.querySelector(".result_years");
+  result_years.innerHTML = `${years}`;
+
+  let result_months = document.querySelector(".result_months");
+  result_months.innerHTML = `${months}`;
+  let result_days = document.querySelector(".result_days");
+  result_days.innerHTML = `${days}`;
+});
