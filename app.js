f73ad5b1b4aeb6fe75a3fb9684c08a3b0c5f197e
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-08-29 19:55:03 +0200

    refactored my app
    - i divided the app into 4 different functions insted of one
    - moved the click function from top to the end of the page since its the last action to be done

--- app.js
+++ app.js
@@ -3,26 +3,35 @@ let inputDay = document.getElementById("day");
 let inputMonth = document.getElementById("month");
 let inputYear = document.getElementById("year");
 
-button.addEventListener("click", function () {
-  let day = inputDay.value;
-  let month = inputMonth.value;
-  let year = inputYear.value;
+/*inputs values*/
 
-  let date = new Date(year, month - 1, day); // month - 1 because month starts from 0
-  let yearOfBirth = date.getFullYear();
-  let monthOfBirth = date.getMonth() + 1; // month + 1 because month starts from 0
-  let dayOfBirth = date.getDate();
-  let currentDate = new Date();
-  let currentYear = currentDate.getFullYear();
-  let currentMonth = currentDate.getMonth() + 1;
-  let currentDay = currentDate.getDate();
-  let years = currentYear - yearOfBirth;
-  let months = currentMonth - monthOfBirth;
-  let days = currentDay - dayOfBirth;
-  let result_years = document.querySelector(".result_years");
-  let result_months = document.querySelector(".result_months");
-  let result_days = document.querySelector(".result_days");
+let day = inputDay.value;
+let month = inputMonth.value;
+let year = inputYear.value;
 
+let date = new Date(year, month - 1, day); // month - 1 because month starts from 0
+
+/*Birth Date*/
+let yearOfBirth = date.getFullYear();
+let monthOfBirth = date.getMonth() + 1; // month + 1 because month starts from 0
+let dayOfBirth = date.getDate();
+
+/*Current Date*/
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let currentMonth = currentDate.getMonth() + 1;
+let currentDay = currentDate.getDate();
+
+/*Results*/
+let years = currentYear - yearOfBirth;
+let months = currentMonth - monthOfBirth;
+let days = currentDay - dayOfBirth;
+
+let result_years = document.querySelector(".result_years");
+let result_months = document.querySelector(".result_months");
+let result_days = document.querySelector(".result_days");
+
+function checkYear() {
   if (currentYear < yearOfBirth) {
     years--;
   } else if (currentYear == yearOfBirth) {
@@ -30,7 +39,9 @@ button.addEventListener("click", function () {
       years--;
     }
   }
+}
 
+function checkMonth() {
   if (currentMonth < monthOfBirth) {
     months += 12;
   } else if (currentMonth == monthOfBirth) {
@@ -38,7 +49,9 @@ button.addEventListener("click", function () {
       months += 12;
     }
   }
+}
 
+function checkDay() {
   if (currentDay < dayOfBirth) {
     days += 30;
   } else if (currentDay == dayOfBirth) {
@@ -46,8 +59,15 @@ button.addEventListener("click", function () {
       days += 30;
     }
   }
+}
 
-  result_years.innerHTML = `${years}`;
-  result_months.innerHTML = `${months}`;
-  result_days.innerHTML = `${days}`;
-});
+function CalculateDate() {
+  checkYear();
+  checkMonth();
+  checkDay();
+  result_years.innerHTML = years;
+  result_months.innerHTML = months;
+  result_days.innerHTML = days;
+}
+
+button.addEventListener("click", CalculateDate);
